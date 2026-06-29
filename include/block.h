93ab3469451b082e8677c6a1fcfee20ad5db85e4
André Rodrigues <andreichihashi1@gmail.com>
2026-06-28 22:37:57 -0300

    adiciona testes de endereco de quadra

--- include/block.h
+++ include/block.h
@@ -6,4 +6,12 @@ typedef struct Block Block;
 Block *block_create(const char *cep, double x, double y, double width, double height);
 void block_destroy(Block *block);
 
+const char *block_cep(const Block *block);
+double block_x(const Block *block);
+double block_y(const Block *block);
+double block_width(const Block *block);
+double block_height(const Block *block);
+
+int block_address_point(const Block *block, char face, double number, double *x, double *y);
+
 #endif
