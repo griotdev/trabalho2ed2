e2fd70e38984b799645430bb2d0e40b6d9b2c8d9
André Rodrigues <andreichihashi1@gmail.com>
2026-06-28 22:36:43 -0300

    valida argumentos na entrada do programa

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
+#include "args.h"
+
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+static void print_usage(FILE *stream) {
+    fputs("Uso: ted [-e path] -f arq.geo [-q consulta.qry] [-v arqvias.via] -o dir\n", stream);
+}
 
-    puts("ted: base inicial do Trabalho 2 de ED2");
+int main(int argc, char **argv) {
+    AppArgs *args = args_create();
+
+    if (args == NULL) {
+        fputs("Erro: memoria insuficiente\n", stderr);
+        return 1;
+    }
+
+    if (!args_parse(args, argc, argv)) {
+        fprintf(stderr, "Erro: %s\n", args_error(args));
+        print_usage(stderr);
+        args_destroy(args);
+        return 1;
+    }
+
+    puts("ted: argumentos validos; processamento ainda nao implementado");
+    args_destroy(args);
     return 0;
 }
