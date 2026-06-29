65bc259bcb6e8ab954597d9f60592c237fdae1f1
André Rodrigues <andreichihashi1@gmail.com>
2026-06-28 22:35:10 -0300

    adiciona testes do parser de argumentos

--- include/args.h
+++ include/args.h
@@ -6,4 +6,13 @@ typedef struct AppArgs AppArgs;
 AppArgs *args_create(void);
 void args_destroy(AppArgs *args);
 
+int args_parse(AppArgs *args, int argc, char **argv);
+
+const char *args_input_dir(const AppArgs *args);
+const char *args_geo_file(const AppArgs *args);
+const char *args_query_file(const AppArgs *args);
+const char *args_via_file(const AppArgs *args);
+const char *args_output_dir(const AppArgs *args);
+const char *args_error(const AppArgs *args);
+
 #endif
